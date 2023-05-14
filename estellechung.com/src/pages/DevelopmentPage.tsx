ea7d87d5b9d4c7a4fd46f0e17e06903cdb746a9d
munchie-babbit <me@estellechung.com>
2023-05-14 13:13:59 -0400

    Initial app set up

---
+++ estellechung.com/src/pages/DevelopmentPage.tsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import Container from '@mui/material/Container';
+
+
+const DevelopmentPage = () => {
+    return ( 
+        <Container>
+                <h1>Dev page</h1>
+                <Footer textColour="white"></Footer>
+        </Container> );
+}
+ 
+export default DevelopmentPage;
