cef0c1a0b01f76a320d9848ff64b948bca0a2a4a
munchie-babbit <estellechung333@gmail.com>
2025-06-16 18:12:49 -0700

    testing addition

--- frontend/src/pages/DevelopmentPage.tsx
+++ frontend/src/pages/DevelopmentPage.tsx
@@ -713,6 +713,7 @@ const DevelopmentPage = () => {
                   }}
                 >
                   <Technology name="Firebase" />
+                  <Technology name="Supabase" />
                   <Technology name="React" />
                   <Technology name="Django" />
                   <Technology name="Google Cloud Platform" />
